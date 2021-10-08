5236c6ab7de9e26204f90bf6d0300e961404a96e
hadi-gharibi <hady.gharibi@gmail.com>
2021-10-08 18:05:10 +0200

    add AlgorithmRegister

--- user_research/utils.py
+++ user_research/utils.py
@@ -1,19 +1,21 @@
 from functools import wraps
-from typing import Dict, Callable
-from .types.algs import AlgProperties
+from typing import Dict, Callable, Set
+from .types.algorithms import StatisticalTestType
 
 
-class Register(object):
+class AlgorithmRegister(object):
     """Singltone class to register functions based on their AlgProperties"""
 
     _instance = None
-    _plugins: Dict[AlgProperties, Callable] = {}
+    _plugins: Dict[StatisticalTestType, Set[Callable]] = {}
 
     def __init__(self, inp=None):
         if inp:
             obj, func = inp
-
-            self._plugins[obj] = func
+            if not self._plugins.get(obj, None):
+                self._plugins[obj] = {func}
+            else:
+                self._plugins[obj].add(func)
 
     def __new__(cls, *args, **kwargs):
         if not isinstance(cls._instance, cls):
@@ -27,6 +29,7 @@ class Register(object):
             return value
 
         return wrapper
+        # return self._plugins[obj]
 
     def __getitem__(self, key):
         return self._plugins[key]
