7a836cd00f44dce3d5f57b074f97a47b81fa735a
hadi-gharibi <hady.gharibi@gmail.com>
2021-10-08 02:58:50 +0200

    add Base class

--- user_research/types/basics.py
+++ user_research/types/basics.py
@@ -1,4 +1,6 @@
 from abc import ABC, abstractmethod
+from functools import wraps
+from varname import argname
 
 
 class Validator(ABC):
@@ -36,3 +38,37 @@ class OptionalBoolean(Validator):
     def validate(self, value):
         if (type(value) != bool) and (value is not None):
             raise ValueError(f"Expected {value!r} to be True or False")
+
+
+class Base:
+    def __hash__(self):
+        return hash(repr(self))
+
+    def __call__(self, func):
+        @wraps(func)
+        def wrapper(*args):
+            value = func(*args)
+            return value
+
+        return self, wrapper
+
+    def __repr__(self):
+        return (
+            f"{self.__class__.__name__}"
+            f"""({', '.join([f'{k}={v!r}' if not k.startswith('_')
+            else f'{k[1:]}={v!r}' for k, v in self.__dict__.items()])})"""
+        )
+
+    def __eq__(self, other):
+        return hash(repr(self)) == hash(repr(other))
+
+    @staticmethod
+    def mutually_exclusive(x, y):
+        x_name, y_name = argname("x", "y")
+        is_x = 1 if x else 0
+        is_y = 1 if y else 0
+        if sum([is_x, is_y]) != 1:
+            raise ValueError(
+                f"You can only have '{x_name}' = {x!r} or '{y_name}' = {y!r}. "
+                f"They are mutually exclusive."
+            )
