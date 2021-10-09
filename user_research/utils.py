1994150d57174ab13a1f1c60ac3eb3ddce6a0e34
hadi-gharibi <hady.gharibi@gmail.com>
2021-10-10 01:21:45 +0200

    add buffer

--- user_research/utils.py
+++ user_research/utils.py
@@ -1,6 +1,7 @@
 from functools import wraps
 from typing import Dict, Callable, Set
 from .types.algorithms import StatisticalTestType
+from copy import deepcopy
 
 
 class AlgorithmRegister(object):
@@ -8,21 +9,27 @@ class AlgorithmRegister(object):
 
     _instance = None
     _plugins: Dict[StatisticalTestType, Set[Callable]] = {}
-
-    def __init__(self, inp=None):
-        if inp:
-            obj, func = inp
-            if not self._plugins.get(obj, None):
-                self._plugins[obj] = {func}
-            else:
-                self._plugins[obj].add(func)
+    _buffer = None
 
     def __new__(cls, *args, **kwargs):
         if not isinstance(cls._instance, cls):
             cls._instance = object.__new__(cls, *args, **kwargs)
+
+        if kwargs:
+            cls._buffer = StatisticalTestType(*args, **kwargs)
+
         return cls._instance
 
     def __call__(self, func):
+        obj = deepcopy(self.__class__._buffer)
+        if obj:
+            if self._plugins.get(obj, None) is None:
+
+                self._plugins[obj] = {func}
+            else:
+                self._plugins[obj].add(func)
+        self.__class__._buffer = None
+
         @wraps(func)
         def wrapper(*args):
             value = func(*args)
